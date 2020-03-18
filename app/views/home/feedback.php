0ceb8a9f60e56e70b0694947d742a03b72a621fd
Danilchenko K <okonmatyler5@gmail.com>
2020-03-18 22:24:43 +0200

    add specialchars

--- app/views/home/feedback.php
+++ app/views/home/feedback.php
@@ -10,9 +10,9 @@
                 <?php for ($i = 0; $i < count($messages); $i++) : ?>
                     <div class="card m-2">
                         <div class="card-body">
-                            <h5 class="card-title"><?= $messages[$i]['email'] ?></h5>
-                            <h6 class="card-subtitle mb-2 text-muted"><?= $messages[$i]['name'] . " at " . $messages[$i]['date_added'] ?></h6>
-                            <p class="card-text"><?= $messages[$i]['text'] ?></p>
+                            <h5 class="card-title"><?= htmlspecialchars($messages[$i]['email'], ENT_QUOTES) ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted"><?=htmlspecialchars($messages[$i]['name'], ENT_QUOTES) . " at " . htmlspecialchars($messages[$i]['date_added'], ENT_QUOTES) ?></h6>
+                            <p class="card-text"><?= htmlspecialchars($messages[$i]['text'], ENT_QUOTES) ?></p>
                         </div>
                     </div>
                 <?php endfor; ?>
